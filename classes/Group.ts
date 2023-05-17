3d0ca1f566bf0150df9d33d71d1b23319282160c
florianln <florian.laurain@gmail.com>
2023-05-17 13:19:19 +0200

    add class group

---
+++ classes/Group.ts
@@ -0,0 +1,14 @@
+class Group {
+    firstname(firstname: string): string {
+        let value: string;
+        value = "John";
+        return value;
+    }
+    lastname(lastname: string): string {
+        let value: string;
+        value = "Doe";
+        return value;
+    }
+}
+
+export default Group;
