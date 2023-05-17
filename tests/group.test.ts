f0563d2e56b2cf4c96abf85d1323e29bd829148b
florianln <florian.laurain@gmail.com>
2023-05-17 14:13:30 +0200

    add tests with faker values

--- tests/group.test.ts
+++ tests/group.test.ts
@@ -1,10 +1,20 @@
 import { describe, expect, test, beforeEach } from '@jest/globals';
 import Group from '../classes/Group';
+import { faker } from '@faker-js/faker'
 
 let group: Group;
 
+function createRandomFirstname() {
+    return faker.person.firstName();
+}
+
+function createRandomLastname() {
+    return faker.person.lastName();
+}
+
 beforeEach(() => {
     group = new Group();
+
 });
 
 describe('Group', () => {
@@ -29,5 +39,19 @@ describe('Group', () => {
     });
     test("Group returns lastname", () => {
         expect(group.lastname("Doe")).toBe("Doe");
+    });
+    test("Group returns firstname and lastname", () => {
+        expect(group.firstname("John")).toBe("John");
+        expect(group.lastname("Doe")).toBe("Doe");
+    });
+    test("Group returns firstname and lastname with random values", () => {
+        expect(group.firstname(createRandomFirstname())).toBeDefined();
+        expect(group.lastname(createRandomLastname())).toBeDefined();
+    });
+    test("Group returns 20 firstname and lastname with random values", () => {
+        for(let i = 0; i < 20; i++) {
+            expect(group.firstname(createRandomFirstname())).toBeDefined();
+            expect(group.lastname(createRandomLastname())).toBeDefined();
+        }
     });
 });
